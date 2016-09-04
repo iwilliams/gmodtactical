7e87c03c764ac4575a5bf73b744fcf441a7cdab2
Ian Williams <ianwilliams633@gmail.com>
2016-09-03 23:15:16 -0700

    fix

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -9,11 +9,11 @@ include("config.lua")
 local tableExists = sql.TableExists('gmt_player')
 
 if not tableExists then
-    sql.begin()
+    sql.Begin()
         sql.query([[
             CREATE TABLE IF NOT EXISTS gmt_player(
                 steamID VARCHAR(25) NOT NULL PRIMARY KEY
             )
         ]])
-    sql.commit()
+    sql.Commit()
 end
