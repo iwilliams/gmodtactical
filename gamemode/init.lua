9586da83e6165907411f7570ef66e550825015c0
Ian Williams <ianwilliams633@gmail.com>
2016-09-03 23:28:04 -0700

    Testing

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -5,15 +5,16 @@ include("shared.lua")
 include("player.lua")
 
 include("config.lua")
+include("libraries/mysqlite/mysqlite.lua")
+MySQLite.initialize(GMT_DBConfig)
 
-local tableExists = sql.TableExists('gmt_player')
+--local tableExists = sql.TableExists('gmt_player')
 
-if not tableExists then
-    sql.Begin()
-        sql.Query([[
-            CREATE TABLE IF NOT EXISTS gmt_player(
-                steamID VARCHAR(25) NOT NULL PRIMARY KEY
-            )
-        ]])
-    sql.Commit()
-end
+--if not tableExists then
+    --sql.begin()
+        --sql.query([[
+            --CREATE TABLE IF NOT EXISTS gmt_player(
+                --teamID VARCHAR(25) NOT NULL PRIMARY KEY
+        --]])
+    --sql.commit()
+--end
