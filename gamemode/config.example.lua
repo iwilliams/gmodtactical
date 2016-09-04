31b247380b9703f2a10ed560ba14f744959e60d6
Ian Williams <ianwilliams633@gmail.com>
2016-09-03 22:25:25 -0700

    Updates to config

---
+++ gamemode/config.example.lua
@@ -0,0 +1,7 @@
+GMT_DBConfig = {
+    EnableMySQL           = true,
+    Host                = "localhost",
+    Username            = "root",
+    Password            = "password",
+    Database_name       = "gmodtactical"
+}
--- gamemode/init.lua
+++ gamemode/init.lua
@@ -4,10 +4,7 @@ AddCSLuaFile("shared.lua")
 include("shared.lua")
 include("player.lua")
 
+include("config.lua")
 include("libraries/mysqlite/mysqlite.lua")
 
-MySQLite.initialize({ EnableMySQL         = true --      :: Bool   - set to true to use MySQL, false for SQLite
-                    , Host                = "localhost" --             :: String - database hostname
-                    , Username            = "root"   --      :: String - database usernam"
-                    , Password            = "" --       :: String - database password (keep away from clients!)
-                    , Database_name       = "gmodtactical" }) --:: String - name of the database
+MySQLite.initialize(GMT_DBConfig)
