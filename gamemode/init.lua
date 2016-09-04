5f79b7595076cc0c6633dfa0714bf416a3ec3173
Ian Williams <ianwilliams633@gmail.com>
2016-09-03 23:07:55 -0700

    DB fix

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -5,6 +5,15 @@ include("shared.lua")
 include("player.lua")
 
 include("config.lua")
-include("libraries/mysqlite/mysqlite.lua")
 
-MySQLite.initialize(GMT_DBConfig)
+local tableExists = sql.TableExists('gmt_player')
+
+if not tableExists then
+    sql.begin()
+        sql.query([[
+            CREATE TABLE IF NOT EXISTS gmt_player(
+                steamID VARCHAR(25) NOT NULL PRIMARY KEY
+            )
+        ]])
+    sql.commit()
+end
